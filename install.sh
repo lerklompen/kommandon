6d3290497dc1621037a894bca3126abd4df843b3
Mattias Bystedt <mattias.bystedt@gmail.com>
2019-09-27 13:28:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,17 +16,32 @@ usage
 
 checkarch=$(uname -m)
 checkos=$(uname -s)
-
 echo $checkarch
 echo $checkos
 
+# TODO - mac: ".bash_profile" Linux: ".bashrc"
+
+bash_string="if [ -f ~/.aliases ]; then\n    . ~/.aliases\n    source ~/.aliases\nfi"
+
+case `grep ".kommandon.sh" ~/.bashrc >/dev/null; echo $?` in
+  0)
+    echo "kommandon.sh not found"
+    echo ${bash_string} >> ~/.bashrc
+    source ~/.bashrc
+  1)
+    echo "kommandon.sh found"
+  *)
+    echo "error finding kommandon.sh"
+esac
+
 # check for file and write or overwrite
-if [ -f ~/check ]; then
+if [ -f ~/.kommandon.sh ]; then
     echo "file found"
-    echo "overwritten" > ~/check
+    echo "alias gam='~/bin/gamadv-xtd3/gam'" > ~/.kommandon.sh
 else
-    touch ~/check
-    echo "write this" > ~/check
+    touch ~/.kommandon.sh
+    echo "#new file" >> ~/.kommandon.sh
+    echo "alias gam='~/bin/gamadv-xtd3/gam'" >> ~/.kommandon.sh
 fi
 
 
