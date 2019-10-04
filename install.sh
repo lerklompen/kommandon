30ada418cd10a89f4a90cec196b6c286ce616544
Mattias Bystedt <mattias.bystedt@gmail.com>
2019-10-04 12:07:22 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,17 @@ checkos=$(uname -s)
 uname -a
 echo $checkos
 
+if [ checkos = "DARWIN" ]; then
+  sed="gsed"
+else
+  sed="sed"
+fi
+
 add_kommandon()
 {
   curl https://raw.githubusercontent.com/lerklompen/kommandon/master/kommandon > ~/.kommandon
+  source ~/gam/.ad_user
+  $sed -i "s/_userid_/$ad_user/g" ~/.kommandon
   # check for dir "gam"
   if [ -d ~/gam ]; then
     echo "dir 'gam' found"
@@ -26,6 +34,17 @@ EOF
 }
 
 usage
+
+# setup AD user and password
+if [ ! -f ~/gam/.ad_user ]; then
+  echo -ne "Please write your AD username and press ENTER:"
+  read ad_user
+  touch ~/gam/.ad_user
+  echo -n "ad_user=${ad_user}" > ~/gam/.ad_user
+  if [ ! -f ~/gam/.pass.txt ]; then
+    nano ~/password.txt && cat ~/password.txt | tr -d "\n" > ~/gam/.pass.txt && rm ~/password.txt && chmod 600 ~/gam/.pass.txt
+  fi
+fi
 
 # TODO - Mac: ".bash_profile" Linux: ".bashrc"
 
@@ -52,7 +71,4 @@ case `grep "kommandon" ~/.bashrc >/dev/null; echo $?` in
 esac
 
 echo "Now type 'exit' and restart terminal"
-
-
-
 
